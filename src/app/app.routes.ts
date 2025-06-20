684d46c5e82fcad4f6581ec4859d7a6e90e34838
hends98 <hendrickcarrera1998@yahoo.com>
2025-06-20 09:28:42 +0800

    Add bootstrap and login, home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './login/login';
+import { Home } from './home/home';
+
+const routes: Routes = [
+    { path: '', component: Login },
+    { path: 'home', component: Home }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRouteModule { }
